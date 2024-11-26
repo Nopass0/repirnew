c6452c2c9e6431dddaa783e70cb30aa31c0dd6fe
Nopass0 <gal.bogdan2015@gmail.com>
2024-11-26 13:09:23 +0500

    afix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://188.120.246.65:3000",
         changeOrigin: true,
         secure: false,
         headers: {
